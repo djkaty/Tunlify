82fe254b7b1d090254c633f4b8f393a517dd0edb
Katy Coe <djkaty@users.noreply.github.com>
2020-03-02 19:19:13 +0100

    Implement tunnel as decorater + subscriber pattern

--- Tunlify.Common/Tunnel.cs
+++ Tunlify.Common/Tunnel.cs
@@ -3,17 +3,82 @@
  */
 
 using System;
-using System.Buffers;
 using System.IO;
-using System.IO.Pipelines;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
-using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace Tunlify.Common
 {
+    public class NetworkStreamEventArgs : EventArgs
+    {
+        public byte[] Data { get; set; }
+    }
+
+    /// <summary>
+    /// Decorator class allowing data in a stream to be captured and dispatched
+    /// </summary>
+    public class ExtendedNetworkStream : Stream
+    {
+        private readonly NetworkStream underlyingStream;
+
+        // Events that fire when bytes are read from or written to the stream
+        public event EventHandler<NetworkStreamEventArgs> ReadBytes;
+        public event EventHandler<NetworkStreamEventArgs> WriteBytes;
+
+        internal ExtendedNetworkStream(NetworkStream stream) => underlyingStream = stream;
+
+        #region Surrogate properties and methods
+        public override void Flush() {
+            underlyingStream.Flush();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count) {
+            return underlyingStream.Read(buffer, offset, count);
+        }
+
+        public override long Seek(long offset, SeekOrigin origin) {
+            return underlyingStream.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value) {
+            underlyingStream.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count) {
+            underlyingStream.Write(buffer, offset, count);
+        }
+
+        public override bool CanRead => underlyingStream.CanRead;
+
+        public override bool CanSeek => underlyingStream.CanSeek;
+
+        public override bool CanWrite => underlyingStream.CanWrite;
+
+        public override long Length => underlyingStream.Length;
+
+        public override long Position {
+            get => underlyingStream.Position;
+            set => underlyingStream.Position = value;
+        }
+#endregion
+
+        public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken) {
+            var bytesRead = await underlyingStream.ReadAsync(buffer, offset, count, cancellationToken);
+
+            ReadBytes?.Invoke(this, new NetworkStreamEventArgs { Data = buffer.Skip(offset).Take(bytesRead).ToArray() });
+            return bytesRead;
+        }
+
+        public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken) {
+            await underlyingStream.WriteAsync(buffer, offset, count, cancellationToken);
+
+            WriteBytes?.Invoke(this, new NetworkStreamEventArgs {Data = buffer.Skip(offset).Take(count).ToArray() });
+        }
+    }
+
     /// <summary>
     /// Class encapsulating two TCP endpoints that forward to each other
     /// </summary>
@@ -37,100 +102,59 @@ namespace Tunlify.Common
 
             // Wait for inbound connection
             var incomingConnection = await listeningSocket.AcceptTcpClientAsync();
-            var incomingStream = incomingConnection.GetStream();
+            var incomingStream = new ExtendedNetworkStream(incomingConnection.GetStream());
 
             Console.WriteLine("Connection accepted from " + Source);
 
             // Create outbound connection
             var outgoingConnection = new TcpClient();
             await outgoingConnection.ConnectAsync(Destination.Address, Destination.Port);
-            var outgoingStream = outgoingConnection.GetStream();
+            var outgoingStream = new ExtendedNetworkStream(outgoingConnection.GetStream());
 
             Console.WriteLine("Connection established with " + Destination);
 
+            // Capture data to log file
             using var logFile = new FileStream("log.bin", FileMode.Create, FileAccess.Write, FileShare.Read);
 
-            // Asynchronously forward the contents of a stream to a pipe
-            async Task forwardStreamToPipeAsync(NetworkStream stream, Pipe pipe) {
+            incomingStream.ReadBytes += async (s, e) => await logFile.WriteAsync(e.Data);
+            outgoingStream.ReadBytes += async (s, e) => await logFile.WriteAsync(e.Data);
+
+            // Start tunneling
+            var srcToDstForwarderTask = incomingStream.CopyToAsync(outgoingStream);
+            var dstToSrcForwarderTask = outgoingStream.CopyToAsync(incomingStream);
+
+            try {
+                // Wait for either side of the connection to be closed, then close the other side
+                var completed = await Task.WhenAny(srcToDstForwarderTask, dstToSrcForwarderTask);
+
+                Console.WriteLine("Connection was closed by the " + (completed == srcToDstForwarderTask ? "client" : "server"));
+            }
+            catch (IOException ex) {
+                // Don't generate an exception if one side terminated the connection
+                if (!(ex.InnerException is SocketException socketEx) || socketEx.ErrorCode != 10053)
+                    throw ex;
+                Console.WriteLine("Connection was terminated");
+            }
+            finally {
+                // Close both sides of the connection
+                // Check Connected property to avoid ObjectDisposedException because one of these will be closed already
+                if (outgoingConnection.Connected)
+                    outgoingConnection.Close();
+                if (incomingConnection.Connected)
+                    incomingConnection.Close();
+
+                // One of these tasks will throw an exception because the transport is already closed
                 try {
-                    int bytesRead;
-                    do {
-                        var buffer = pipe.Writer.GetMemory(65536);
-                        bytesRead = await stream.ReadAsync(buffer);
-                        pipe.Writer.Advance(bytesRead);
-                    } while (!(await pipe.Writer.FlushAsync()).IsCompleted && bytesRead > 0);
-
-                    Console.WriteLine("Stream exhausted");
+                    await Task.WhenAll(srcToDstForwarderTask, dstToSrcForwarderTask);
                 }
-
-                // This exception occurs when we close the stream from outside the task
                 catch (IOException ex) {
                     if (!(ex.InnerException is SocketException socketEx) || socketEx.ErrorCode != 995)
                         throw ex;
-                    Console.WriteLine("Underlying socket was closed by us");
+                    Console.WriteLine("Streams closed");
                 }
 
-                // Notify the pipe reader that there is no further data coming
-                finally {
-                    pipe.Writer.Complete();
-                }
+                Console.WriteLine("Connection closed");
             }
-
-            // Consume the contents of a pipe
-            async Task pipeConsumer(Pipe pipe, NetworkStream dest) {
-                ReadResult readResult;
-                do {
-                    readResult = await pipe.Reader.ReadAsync();
-                    var block = readResult.Buffer.ToArray();
-
-                    try {
-                        await Task.WhenAll(dest.WriteAsync(block).AsTask(), logFile.WriteAsync(block).AsTask());
-                    }
-
-                    // ObjectDisposedException occurs when trying to write a stream that has been closed
-                    catch (Exception ex) {
-                        if (!(ex is ObjectDisposedException))
-                            throw ex;
-                    }
-
-                    await logFile.FlushAsync();
-
-                    pipe.Reader.AdvanceTo(readResult.Buffer.End);
-                } while (!readResult.IsCompleted);
-
-                Console.WriteLine("Pipe complete");
-            }
-
-            // Create a pipe for each side of the connection
-            var sourcePipe = new Pipe();
-            var destPipe = new Pipe();
-
-            // Set up stream-to-pipe forwarders
-            var srcToPipeTask = forwardStreamToPipeAsync(incomingStream, sourcePipe);
-            var dstToPipeTask = forwardStreamToPipeAsync(outgoingStream, destPipe);
-
-            // Set up pipe consumers
-            var srcPipeConsumer = pipeConsumer(sourcePipe, outgoingStream);
-            var dstPipeConsumer = pipeConsumer(destPipe, incomingStream);
-
-            // Wait for either side of the connection to be closed, then close the other side
-            var completed = await Task.WhenAny(srcToPipeTask, dstToPipeTask);
-
-            Console.WriteLine("Connection was closed by the " + (completed == srcToPipeTask ? "client" : "server"));
-
-            // Close both sides of the connection, causing IOException in the task thread
-            // Check Connected property to avoid ObjectDisposedException because one of these will be closed already
-            if (outgoingConnection.Connected)
-                outgoingConnection.Close();
-            if (incomingConnection.Connected)
-                incomingConnection.Close();
-
-            await Task.WhenAll(srcToPipeTask, dstToPipeTask);
-
-            // Wait for both pipes to be emptied
-            await Task.WhenAll(srcPipeConsumer, dstPipeConsumer);
-
-            Console.WriteLine("Connection closed");
         }
     }
 }
